7f72fe5d8e3851e4b186732effcc069eaefb8e40
Blessan <blessan.s@terrificminds.com>
2024-03-07 13:17:47 +0530

    Adding index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const Shopify = require('shopify-api-node');
+
+const PORT = 5000
+
+
+app.get('/orders',async(req,res)=> {
+
+  const headers = req.headers;
+      const shopName = headers['shopname'];
+      const accessToken = headers['accesstoken'];
+      const id = headers['id'];
+
+  const shopify = new Shopify({
+    shopName: shopName,
+    accessToken: accessToken
+  });
+
+  
+  console.log("id is", id);
+  
+  const query = `{
+    order(id: "gid://shopify/Order/5514302259495") {
+      currencyCode
+      discountCode
+      displayFulfillmentStatus
+      fullyPaid
+      paymentGatewayNames
+      createdAt
+      totalShippingPrice
+      totalTax
+      paymentCollectionDetails {
+        additionalPaymentCollectionUrl
+      }
+    }
+  }  
+  `;
+
+  const products = await shopify.graphql(query);
+  res.json(products);
+
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running at ${PORT}.....`)
+})
+
+
+
